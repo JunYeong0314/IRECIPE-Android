4cea949ecce173e30c629db629cd9de7def77c08
Dayun Lee <wohd7877@naver.com>
2024-01-26 15:31:16 +0900

    [feat] 온보딩 구현

---
+++ IRecipe/app/src/main/java/com/umcproject/irecipe/presentation/util/onboarding/OnboardingOneFragment.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.umcproject.irecipe.R
+
+
+class OnboardingOneFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_onboarding_one, container, false)
+    }
+}
---
+++ IRecipe/app/src/main/java/com/umcproject/irecipe/presentation/util/onboarding/OnboardingTwoFragment.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.umcproject.irecipe.R
+
+
+class OnboardingTwoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_onboarding_two, container, false)
+    }
+}
