280b7527d175229a3d8da2ce48cf474349d210cd
Dayun Lee <wohd7877@naver.com>
2024-01-29 19:48:49 +0900

    [fix] 챗봇 ui 수정

--- IRecipe/app/src/main/java/com/umcproject/irecipe/presentation/ui/chat/ChatFragment.kt
+++ IRecipe/app/src/main/java/com/umcproject/irecipe/presentation/ui/chat/ChatFragment.kt
@@ -81,14 +81,14 @@ class ChatFragment(): BaseFragment<FragmentChatBotBinding>() {
         }
 
         //뒤로 가기
-        binding.ibtnBack.setOnClickListener{
-            /*(activity as MainActivity).binding.btmMain.visibility = View.VISIBLE //바텀바 다시 등장
-            (context as MainActivity).supportFragmentManager.beginTransaction() //homeFragment로 이동
-                .replace(R.id.fv_main, HomeFragment())
-                .commitAllowingStateLoss()*/
-            showBottomBar()
-            popFragment(requireActivity())
-        }
+//        binding.ibtnBack.setOnClickListener{
+//            /*(activity as MainActivity).binding.btmMain.visibility = View.VISIBLE //바텀바 다시 등장
+//            (context as MainActivity).supportFragmentManager.beginTransaction() //homeFragment로 이동
+//                .replace(R.id.fv_main, HomeFragment())
+//                .commitAllowingStateLoss()*/
+//            showBottomBar()
+//            popFragment(requireActivity())
+//        }
     }
 
     private fun addToChat(message: String, sentBy: String) { //채팅 쓰는 쪽
