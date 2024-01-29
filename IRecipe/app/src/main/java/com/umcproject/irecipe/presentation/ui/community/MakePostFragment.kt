8a5f410f3f41d3a993f6e8019a02aadaf6cfa8e1
whthdbs03 <121880741+whthdbs03@users.noreply.github.com>
2024-01-30 03:36:37 +0900

    [feat] 커뮤니티 ui 추가 구성 및 화면 전환 변경

--- IRecipe/app/src/main/java/com/umcproject/irecipe/presentation/ui/community/MakePostFragment.kt
+++ IRecipe/app/src/main/java/com/umcproject/irecipe/presentation/ui/community/MakePostFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
 import com.umcproject.irecipe.R
 import com.umcproject.irecipe.databinding.FragmentMakePostBinding
 import com.umcproject.irecipe.presentation.util.BaseFragment
@@ -31,21 +32,22 @@ class MakePostFragment : BaseFragment<FragmentMakePostBinding>() {
 
         binding.btnPost.setOnClickListener {
             requireActivity().supportFragmentManager
-                .popBackStack()
-
-            requireActivity().supportFragmentManager
-                .beginTransaction().replace(R.id.fv_main,PostFragment())
-                .addToBackStack(null)
-                .commitAllowingStateLoss()
+                .beginTransaction().remove(this).commit()
+            requireActivity().supportFragmentManager.popBackStack()
         }
 
         binding.btnFoodType.setOnClickListener {
+            it.isSelected = true
             val modal = ModalBottomSheetFoodType()
             modal.setStyle(DialogFragment.STYLE_NORMAL, R.style.RoundCornerBottomSheetDialogTheme)
             modal.show(childFragmentManager, ModalBottomSheetFoodType.TAG)
+            modal.dialog?.setOnDismissListener {
+                binding.btnFoodType.isSelected = false
+            }
         }
 
         binding.btnLevel.setOnClickListener {
+            it.isSelected = true
             val modal = ModalBottomSheetLevel()
             modal.setStyle(DialogFragment.STYLE_NORMAL, R.style.RoundCornerBottomSheetDialogTheme)
             modal.show(childFragmentManager, ModalBottomSheetLevel.TAG)
@@ -54,7 +56,7 @@ class MakePostFragment : BaseFragment<FragmentMakePostBinding>() {
         wordsLimit(binding.tvTitle, binding.tvTitleCnt, 20)
         wordsLimit(binding.tvSubtitle, binding.tvSubtitleCnt, 50)
     }
-    fun wordsLimit(editText: EditText, cntView : TextView, limit: Int) {
+    private fun wordsLimit(editText: EditText, cntView : TextView, limit: Int) {
         editText.addTextChangedListener(object : TextWatcher { // 글자수 제한
             var maxText = ""
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -77,11 +79,14 @@ class MakePostFragment : BaseFragment<FragmentMakePostBinding>() {
     fun updateLevelButtonText(text: String) {
         val button = binding.btnLevel
         button.text = text
+        button.isSelected = false
         val params = button.layoutParams
         params.width = ViewGroup.LayoutParams.WRAP_CONTENT
         button.layoutParams = params
     }
     fun updateTypeButtonText(text: String) {
-        binding.btnFoodType.text = text
+        val button = binding.btnFoodType
+        button.text = text
+        button.isSelected = false
     }
 }
