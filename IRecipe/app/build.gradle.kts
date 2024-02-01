808c7668adbb4eecde98b679575500d4b9682025
whthdbs03 <121880741+whthdbs03@users.noreply.github.com>
2024-02-02 06:01:53 +0900

    [Feat] 커뮤니티 ui 추가

--- IRecipe/app/build.gradle.kts
+++ IRecipe/app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.cardview:cardview:1.0.0")
     // ViewModel
     implementation ("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.1")
     implementation ("androidx.activity:activity-ktx:1.7.2")
---
+++ IRecipe/app/src/main/java/com/umcproject/irecipe/presentation/ui/community/CommunityPostAdapter.kt
@@ -0,0 +1,26 @@
+package com.umcproject.irecipe.presentation.ui.community
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.umcproject.irecipe.databinding.ItemPostBinding
+
+class CommunityPostAdapter:RecyclerView.Adapter<CommunityPostAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CommunityPostAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: CommunityPostAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class ViewHolder(val binding: ItemPostBinding):RecyclerView.ViewHolder(binding.root){
+
+    }
+}
