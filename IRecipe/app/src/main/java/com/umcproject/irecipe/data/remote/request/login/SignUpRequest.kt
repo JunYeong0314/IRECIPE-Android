7b7da7eda0777452b0fd8143c4e3dca3131feda7
JunYeong Kim <96372487+JunYeong0314@users.noreply.github.com>
2024-02-14 14:56:28 +0900

    [Fix] mapper 함수 추가

--- IRecipe/app/src/main/java/com/umcproject/irecipe/data/remote/request/login/SignUpRequest.kt
+++ IRecipe/app/src/main/java/com/umcproject/irecipe/data/remote/request/login/SignUpRequest.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.Json
 
 data class SignUpRequest(
     @field:Json(name = "age")
-    val age: Int?,
+    val age: String?,
     @field:Json(name = "allergyList")
     val allergyList: List<Int?>?,
     @field:Json(name = "gender")
