a90b9f0643d289d0032606396c41675ea6aeabf0
whthdbs03 <121880741+whthdbs03@users.noreply.github.com>
2024-02-19 21:30:43 +0900

    [Feat] 좋아요 구현

--- IRecipe/app/src/main/java/com/umcproject/irecipe/presentation/ui/community/post/PostFragment.kt
+++ IRecipe/app/src/main/java/com/umcproject/irecipe/presentation/ui/community/post/PostFragment.kt
@@ -32,6 +32,7 @@ class PostFragment(
     private val currentScreen: String
 ) : BaseFragment<FragmentPostBinding>() {
     private val viewModel: CommunityViewModel by viewModels()
+    private val postViewModel: PostViewModel by viewModels()
 
     companion object{
         const val TAG = "PostFragment"
@@ -60,6 +61,7 @@ class PostFragment(
         }
 
         onClickReview() // Q&A, 리뷰글 버튼 이벤트
+        onClickLike() // 게시글 좋아요 버튼 이벤트
     }
 
     private fun initView(){
@@ -89,6 +91,19 @@ class PostFragment(
             isLike = it
         }
     }
+    private fun initLikes(){
+        val postInfo = viewModel.getPostInfo()
+
+        postInfo?.isLike?.let {
+            if(it) binding.ivHeart.setImageResource(R.drawable.ic_heart)
+            else binding.ivHeart.setImageResource(R.drawable.ic_heart_empty)
+            isLike = it
+        }
+        postInfo?.likes?.let {
+            binding.tvHeartCnt.text = it.toString()
+        }
+
+    }
 
     override fun onDestroy() {
         super.onDestroy()
@@ -113,7 +128,30 @@ class PostFragment(
     }
 
     private fun onClickLike(){
-        
+        binding.ivHeart.setOnClickListener {
+            if (isLike==true) { // 좋아요 해제
+                postViewModel.postUnLike(postId)
+                postViewModel.unlikeState.observe(viewLifecycleOwner) {state ->
+//                    state?.let {
+//                        if(state == 200) Snackbar.make(requireView(), "좃앟요 삭제됨", Snackbar.LENGTH_SHORT).show()
+//                    }
+                }
+                binding.ivHeart.setImageResource(R.drawable.ic_heart_empty)
+                binding.tvHeartCnt.text = (binding.tvHeartCnt.text.toString().toInt() - 1).toString()
+
+            } else { // 좋아요
+                postViewModel.postLike(postId)
+                postViewModel.likeState.observe(viewLifecycleOwner) {state ->
+                    state?.let {
+                        if(state == 200) Snackbar.make(requireView(), getString(R.string.confirm_set_like), Snackbar.LENGTH_SHORT).show()
+
+                    }
+                }
+                binding.ivHeart.setImageResource(R.drawable.ic_heart)
+                binding.tvHeartCnt.text = (binding.tvHeartCnt.text.toString().toInt() + 1).toString()
+            }
+            isLike = !isLike!!
+        }
     }
 
     private fun mapperToCategory(category: String): String {
