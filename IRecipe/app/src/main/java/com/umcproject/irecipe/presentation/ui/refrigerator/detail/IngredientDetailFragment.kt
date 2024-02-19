786f7dc0d1a8ffa26e45399d8fa12f2cd60a042e
Dayun Lee <wohd7877@naver.com>
2024-02-19 19:47:14 +0900

    [Feat] 냉장고 삭제 서버 연결

--- IRecipe/app/src/main/java/com/umcproject/irecipe/presentation/ui/refrigerator/detail/IngredientDetailFragment.kt
+++ IRecipe/app/src/main/java/com/umcproject/irecipe/presentation/ui/refrigerator/detail/IngredientDetailFragment.kt
@@ -4,24 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.google.android.material.snackbar.Snackbar
 import com.umcproject.irecipe.R
 import com.umcproject.irecipe.databinding.FragmentIngredientDetailBinding
 import com.umcproject.irecipe.domain.model.Ingredient
+import com.umcproject.irecipe.presentation.ui.community.CommunityViewModel
 import com.umcproject.irecipe.presentation.ui.refrigerator.RefrigeratorFragment
+import com.umcproject.irecipe.presentation.ui.refrigerator.RefrigeratorViewModel
 import com.umcproject.irecipe.presentation.ui.refrigerator.Type
 import com.umcproject.irecipe.presentation.ui.refrigerator.process.RefrigeratorProcessFragment
 import com.umcproject.irecipe.presentation.util.BaseFragment
+import com.umcproject.irecipe.presentation.util.Util
 import com.umcproject.irecipe.presentation.util.Util.getEngResourceId
 import com.umcproject.irecipe.presentation.util.Util.mapperToKorIngredientCategory
 import com.umcproject.irecipe.presentation.util.Util.mapperToKorIngredientType
 import com.umcproject.irecipe.presentation.util.Util.popFragment
 import com.umcproject.irecipe.presentation.util.Util.showVerticalFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class IngredientDetailFragment(
     private val ingredient: Ingredient,
     private val onClickBackBtn: (String) -> Unit,
-    private val currentFragment: String
+    private val currentFragment: String,
+    private val onIngredientCallBack: () -> Unit
 ): BaseFragment<FragmentIngredientDetailBinding>() {
+    private val viewModel: RefrigeratorViewModel by viewModels()
 
     companion object{
         const val TAG = "IngredientFragment"
@@ -37,7 +46,18 @@ class IngredientDetailFragment(
         super.onViewCreated(view, savedInstanceState)
 
         setIngredientInfo() // 초기 정보 설정
-        binding.btnConfirm.setOnClickListener { popFragment(requireActivity()) } // 확인 클릭 이벤트
+        binding.btnConfirm.setOnClickListener {
+            viewModel.deleteState.observe(viewLifecycleOwner){
+            if(it == 200){
+                Util.popFragment(requireActivity())
+                onIngredientCallBack()
+                Snackbar.make(requireView(), "삭제에 성공하였습니다!", Snackbar.LENGTH_SHORT).show()
+            }else{
+                Snackbar.make(requireView(), "게시글을 찾지 못했습니다.", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+
+            viewModel.deleteIngredient(ingredient.id)} // 확인 클릭 이벤트
         binding.btnModify.setOnClickListener {
             showVerticalFragment(
                 R.id.fv_main, requireActivity(),
