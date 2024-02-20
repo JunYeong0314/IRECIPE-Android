06dde32faf332e09166aa1d046d84a64cec0aded
JunYeong Kim <96372487+JunYeong0314@users.noreply.github.com>
2024-02-21 01:19:02 +0900

    [Fix] 버전명 변경

--- IRecipe/app/build.gradle.kts
+++ IRecipe/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.umcproject.eyerecipe"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
